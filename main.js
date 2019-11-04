3ca2b94660cb427ab522bc3143006f75baf27987
Chance Smith <chancesmithb@gmail.com>
2019-11-04 08:39:59 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
-element.classList.contains(class);
-
 let isTableHeaderFound = false;
 
 do{
   if(document.querySelector('.table-list-header-toggle')) isTableHeaderFound = true
 } while(!isTableHeaderFound)
 
-let pointsCount = 0
-document.querySelectorAll('.IssueLabel').forEach(function(label){
-  if(label.innerText.includes('size')) pointsCount += parseInt(label.innerText.replace('size: ', ''))
-})
-console.log(pointsCount)
-const countElement = document.createElement('span')
+if(isTableHeaderFound) {
+  let pointsCount = 0
+  document.querySelectorAll('.IssueLabel').forEach(function(label){
+    if(label.innerText.includes('size')) pointsCount += parseInt(label.innerText.replace('size: ', ''))
+  })
+  console.log(pointsCount)
+  const countElement = document.createElement('span')
 
-countElement.textContent = `Points: ${pointsCount}`
-const githubTableHeader = document.querySelector('.table-list-header-toggle')
-githubTableHeader.appendChild(countElement)
+  countElement.textContent = `Points: ${pointsCount}`
+  const githubTableHeader = document.querySelector('.table-list-header-toggle')
+  githubTableHeader.appendChild(countElement)
+}
